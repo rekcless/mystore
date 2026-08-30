0cf9712d30838fa24419320205632bca3071d89a
rekcless <wakiedimam@gmail.com>
2026-08-30 10:57:01 +0700

    Add files via upload

---
+++ js/firebase-admin.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore, collection, getDocs, getDoc, addDoc, doc, updateDoc, deleteDoc, serverTimestamp, query, orderBy, setDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export { onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, collection, getDocs, getDoc, addDoc, doc, updateDoc, deleteDoc, serverTimestamp, query, orderBy, setDoc, ref, uploadBytes, getDownloadURL };
